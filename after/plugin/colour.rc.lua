b70aeca970c069344a539969d4aa2590d655110f
Sammyalhashe <sammy@salh.xyz>
2025-06-29 19:29:00 +0000

    Ignoring the colorscheme autocommand unless asked for.

--- after/plugin/colour.rc.lua
+++ after/plugin/colour.rc.lua
@@ -69,55 +69,55 @@ else
 end
 
 --> Autocmd that keeps wezterm and neovim in sync on unix
-vim.api.nvim_create_autocmd("ColorScheme", {
-  group = vim.api.nvim_create_augroup("wezterm_colorscheme", { clear = true }),
-  callback = function(args)
-    local colorschemes = {
-      ["carbonfox"] = "carbonfox",
-      ["dayfox"] = "dayfox",
-      ["dawnfox"] = "dawnfox",
-      ["duskfox"] = "duskfox",
-      ["terafox"] = "terafox",
-      ["nightfox"] = "nightfox",
-      ["monokai-nightasty"] = "MonokaiPro (Gogh)",
-      ["gruvbox"] = "GruvboxDark",
-      ["duckbones"] = "duckbones",
-      ["vimbones"] = "vimbones"
-      -- add more color schemes here ...
-    }
-    local colorscheme = colorschemes[args.match]
-    if not colorscheme then
-      return
-    end
-    -- Write the colorscheme to a file
-    -- local colorscheme_file = "$HOME/.colorscheme"
+if vim.g.disable_wezterm_colorscheme_auto ~= nil and not vim.g.disable_wezterm_colorscheme_auto then
+    vim.api.nvim_create_autocmd("ColorScheme", {
+      group = vim.api.nvim_create_augroup("wezterm_colorscheme", { clear = true }),
+      callback = function(args)
+        local colorschemes = {
+          ["carbonfox"] = "carbonfox",
+          ["dayfox"] = "dayfox",
+          ["dawnfox"] = "dawnfox",
+          ["duskfox"] = "duskfox",
+          ["terafox"] = "terafox",
+          ["nightfox"] = "nightfox",
+          ["monokai-nightasty"] = "MonokaiPro (Gogh)",
+          ["gruvbox"] = "GruvboxDark",
+          ["duckbones"] = "duckbones",
+          ["vimbones"] = "vimbones"
+          -- add more color schemes here ...
+        }
+        local colorscheme = colorschemes[args.match]
+        if not colorscheme then
+          return
+        end
+        -- Write the colorscheme to a file
+        -- local colorscheme_file = "$HOME/.colorscheme"
 
-    if vim.g.wsl ~= 0 then
-        --> NOTE A string can also be inside `[[]]` in lua
-        local userprofile = vim.fn.system([[cmd.exe /C "echo %USERPROFILE%" 2>/dev/null | tr -d '\r']])
-        local wezterm_path = vim.fn.system("wslpath " .. userprofile)
-        --> NOTE Might need to change this. I currently edit in whatever the
-        --> "root" is in powershell.
-        --> TODO Append actual wezterm path
-        wezterm_colorscheme_file = wezterm_path
-    end
-    local filename = vim.fn.expand(wezterm_colorscheme_file)
-    assert(type(filename) == "string")
-    local file = io.open(filename, "w")
-    assert(file)
-    file:write(colorscheme)
-    file:close()
-    vim.notify("Setting WezTerm color scheme to " .. colorscheme, vim.log.levels.INFO)
-    
-    filename = vim.fn.expand(neovim_colorscheme_file)
-    assert(type(filename) == "string")
-    file = io.open(filename, "w")
-    assert(file)
-    file:write(args.match)
-    file:close()
-  end,
-})
-
-
+        if vim.g.wsl ~= 0 then
+            --> NOTE A string can also be inside `[[]]` in lua
+            local userprofile = vim.fn.system([[cmd.exe /C "echo %USERPROFILE%" 2>/dev/null | tr -d '\r']])
+            local wezterm_path = vim.fn.system("wslpath " .. userprofile)
+            --> NOTE Might need to change this. I currently edit in whatever the
+            --> "root" is in powershell.
+            --> TODO Append actual wezterm path
+            wezterm_colorscheme_file = wezterm_path
+        end
+        local filename = vim.fn.expand(wezterm_colorscheme_file)
+        assert(type(filename) == "string")
+        local file = io.open(filename, "w")
+        assert(file)
+        file:write(colorscheme)
+        file:close()
+        vim.notify("Setting WezTerm color scheme to " .. colorscheme, vim.log.levels.INFO)
+        
+        filename = vim.fn.expand(neovim_colorscheme_file)
+        assert(type(filename) == "string")
+        file = io.open(filename, "w")
+        assert(file)
+        file:write(args.match)
+        file:close()
+      end,
+    })
+end
 
 vim.cmd("colorscheme " .. colorscheme)
